a5d3a39bf0a16af482d5e0a4e4f5a65cf65de38a
Rias Gauri Nurhasanah <19102212@ittelkom-pwt.ac.id>
2021-10-25 07:31:39 +0700

    mvc

---
+++ app/controllers/About.php
@@ -0,0 +1,15 @@
+<?php
+
+class About {
+    public function page() 
+    {
+        echo '/about/page';
+    }
+}
+
+class Index {
+    public function page() 
+    {
+        echo 'faizal';
+    }
+}
